2302968bd341b994bf30db9c9f500ebe4d1b3e8b
joanarau-schweizer <joan.arau@me.com>
2020-06-15 19:10:53 +0100

    /

--- pairs_many.py
+++ pairs_many.py
@@ -8,12 +8,38 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
 from itertools import combinations
+from statsmodels.stats.stattools import durbin_watson, jarque_bera
 
+pd.set_option('display.max_columns', 500)
 # ticker list
-symbList = ['NEE', 'D', 'DUK', 'SO', 'AEP', 'SRE', 'EXC', 'XEL', 'WEC', 'ES', 'ED', 'PEG', 'FE', 'AWK', 'EIX', 'DTE', 'PPL', 'ETR', 'AEE', 'CMS', 'EVRG', 'ATO', 'LNT', 'NI', 'PNW', 'AES', 'CNP', 'NRG']
 
-start_date = '2015/01/01'
-end_date = datetime.now()
+#XLU
+#symbList = ['NEE', 'D', 'DUK', 'SO', 'AEP', 'SRE', 'EXC', 'XEL', 'WEC', 'ES', 'ED', 'PEG', 'FE', 'AWK', 'EIX', 'DTE', 'PPL', 'ETR', 'AEE', 'CMS', 'EVRG', 'ATO', 'LNT', 'NI', 'PNW', 'AES', 'CNP', 'NRG']
+
+#XLE
+symbList =['CVX', 'XOM', 'COP', 'PSX', 'EOG', 'VLO', 'WMB', 'SLB', 'KMI', 'MPC', 'PXD', 'OXY', 'OKE', 'HES', 'CXO', 'BKR', 'HAL', 'COG', 'FANG', 'NOV', 'HFC', 'MRO', 'DVN', 'APA', 'NBL', 'FTI', 'HP']
+
+#XLRE
+#symbList =['AMT', 'CCI', 'PLD', 'EQIX', 'DLR', 'SBAC', 'PSA', 'AVB', 'EQR', 'SPG', 'WELL', 'ARE', 'O', 'ESS', 'WY', 'BXP', 'CBRE', 'MAA', 'PEAK', 'DRE', 'VTR', 'EXR', 'UDR', 'HST', 'REG', 'IRM', 'VNO', 'FRT', 'AIV', 'KIM', 'SLG']
+
+#XLC
+#symbList =['FB', 'GOOGL', 'GOOG', 'TMUS', 'EA', 'NFLX', 'CHTR', 'ATVI', 'DIS', 'VZ', 'CMCSA', 'T', 'TWTR', 'TTWO', 'OMC', 'CTL', 'FOXA', 'VIAC', 'DISCK', 'IPG', 'DISH', 'LYV', 'FOX', 'NWSA', 'DISCA', 'NWS']
+
+#XLY
+# symbList =['AMZN', 'HD', 'MCD', 'NKE', 'SBUX', 'LOW', 'BKNG', 'TJX', 'TGT', 'DG', 'ROST', 'EBAY', 'ORLY', 'GM', 'YUM', 'AZO', 'MAR', 'CMG', 'HLT', 'F', 'DLTR', 'VFC', 'APTV', 'BBY', 'DHI', 'LVS', 'LEN', 'TIF', 'ULTA', 'TSCO', 'KMX', 'GRMN', 'GPC', 'NVR', 'EXPE', 'DRI', 'HAS', 'AAP', 'WYNN', 'MGM', 'LKQ', 'WHR', 'PHM', 'RCL', 'CCL', 'BWA', 'MHK', 'NWL', 'LEG', 'TPR', 'RL', 'HBI', 'PVH', 'HOG', 'HRB', 'NCLH', 'KSS', 'LB', 'CPRI', 'JWN', 'UAA', 'GPS']
+
+#XLP
+#symbList = ['PG', 'PEP', 'KO', 'WMT', 'MDLZ', 'MO', 'COST', 'PM', 'CL', 'KMB', 'EL', 'GIS', 'WBA', 'STZ', 'SYY', 'KR', 'CLX', 'MNST', 'HSY', 'ADM', 'MKC',  'TSN', 'CHD', 'CAG', 'K', 'SJM', 'HRL',  'CPB',  'TAP', 'COTY']
+
+
+#symbList = ['SPY','XLU']
+
+
+
+start_date = '2010/01/01'
+end_date = '2015/01/01'
+
+#end_date = datetime.now()
 
 def get_data(symbList=symbList,start_date=start_date,end_date=end_date):
 
@@ -21,7 +47,7 @@ def get_data(symbList=symbList,start_date=start_date,end_date=end_date):
     for i in symbList:
         print('Loading data: ',i)
         dt = data.DataReader(i, "yahoo", start=start_date, end=end_date)
-        dt.rename(columns={'Adj Close': 'price'}, inplace=True)
+        dt.rename(columns={'Close': 'price'}, inplace=True)
         df_list.append(dt)
 
 
@@ -31,7 +57,12 @@ def get_data(symbList=symbList,start_date=start_date,end_date=end_date):
 
     s_list = []
     for df in df_list:
-        s = df['price']
+
+        log = False
+        if log == True:
+            s = np.log(df['price'])
+        else:
+            s = df['price']
         s_list.append(s)
     df =  pd.concat(s_list, axis=1, keys=symbList).dropna()
     print(df.head())
@@ -44,9 +75,14 @@ def stats(x,y):
     df1 = pd.DataFrame({'x':x,'y':y})
     est = sm.OLS(df1.y, df1.x)
     est = est.fit()
+
+    dw = durbin_watson(est.resid)
+    jb = jarque_bera(est.resid)
+    #print(dw)
     df1['hr'] = est.params[0]
     hr= df1['hr'].mean()
     hrr = round(hr)
+    #print(hrr)
     df1['spread'] = df1.y - (df1.x * hrr)
 
     mean = df1.spread.mean()
@@ -66,7 +102,14 @@ def stats(x,y):
 
     halflife = round(-np.log(2) / res.params[1], 0)
 
-    return [hrr,df1.hr.std(),mean,std,halflife,cadf,hrr-hr,df1]
+    df1['next_s'] = df1.spread.shift(-1)
+    df1['cross'] = (
+            ((df1.spread >= mean) & (df1.next_s < mean)) |
+            ((df1.next_s > mean) & (df1.spread <= mean)) |
+            (df1.spread == mean))
+
+    cross = df1.cross.sum()
+    return [hrr,df1.hr.std(),mean,std,halflife,cadf,hrr-hr,df1,dw,est,jb,cross]
 
 
 def plot1(x,y,i):
@@ -106,6 +149,7 @@ print(combs)
 
 ind = 1
 
+dict_list = []
 for i in combs:
     print('##########')
     print(ind,'/',len(combs))
@@ -117,25 +161,37 @@ for i in combs:
     y = df[i[1]]
     st = stats(x,y)
 
+    #plot2(x=x, y=y, i=i)
 
+    if abs(st[5][0]) > abs(st[5][4]['1%']) and st[4]>0  and st[0] >0 and st[4] < 50:#and st[5][1] < (0.1/len(combs)) and st[8]>1.5 and st[8] < 2.5 :
 
-    if abs(st[5][0]) > abs(st[5][4]['5%']) and st[4]>0:
+        print(st[9].summary())
 
 
         print('mean : ',st[2],' std : ',st[3])
-        print('Hedge Ratio mean : ',st[0],' std : ',st[1],' round : ',st[-2])
+        print('Hedge Ratio mean : ',st[0],' std : ',st[1],' round : ',st[6])
         print ('Augmented Dickey Fuller test statistic =',st[5][0])
         print ('Augmented Dickey Fuller p-value =',st[5][1])
         print ('Augmented Dickey Fuller 1%, 5% and 10% test statistics =',st[5][4])
 
 
+
         print  ('Halflife = ',st[4])
+
+        print('Mean crossings = ', st[11])
 
         #plot1(x=x,y=y,i=i)
         plot2(x=x, y=y, i=i)
-        plot3(st[-1],st[2],st[3],i)
+        plot3(st[7],st[2],st[3],i)
+
+        dic = {'pair':i,'mean':st[2],'std':st[3],'adf_test':st[5][0],'adf_p_value':st[5][1],'hedge_ratio_round':st[0],'hedge_ratio_error':st[6],'halflife':st[4],'Durbin':st[8],'cross': st[11]}
+        dict_list.append(dic)
 
     else:
         print('Failed Test')
     #breakpoint()
 
+df = pd.DataFrame(dict_list).sort_values(by=['halflife'], ascending=True)
+
+
+print(df)
